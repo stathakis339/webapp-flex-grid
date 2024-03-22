d3db8bfcd5eddc4fc6a81135d23ff05a6db8c448
Lefteris Stathakis <stathakislefteris@outlook.com>
2024-03-22 22:57:56 +0200

    Small update

--- javascript/objects/examble3/app.js
+++ javascript/objects/examble3/app.js
@@ -1,6 +1,6 @@
-const json = require('./data.json');  // Imports the contents of a JSON file
+const {readFileSync} = require('fs');   //call fs , bring me readFileSync //const json = require('./data.json');   Imports the contents of a JSON file
 
-const data = json.data;               // Extract "data" and assign it to a new variable called data
+//const data = json.data;               // Extract "data" and assign it to a new variable called data
 
 const args = process.argv;            //The process.argv array contains command-line arguments passed to the Node.js script. Args will hold an array of command-line arguments
 
@@ -8,8 +8,12 @@ const args = process.argv;            //The process.argv array contains command-
 
 let command = args[2];                // Extract the third argument and asign it the command varieble
 
-if (command === 'showAll'){           // Check if the value of command is equal to the string "showall"
-data.forEach(val=>{           
-  console.log(val.name);              // Will show the names from the data array 
-});
-}
+if ( command === 'showAll'){
+  let results = readFileSync('data.json','utf-8');
+  console.log(JSON.parse(results));                // It will show the names from the data array
+} 
+
+// if (command === `showOne`){
+//   results = readFileSync
+// }
+
